972bf2f38334ae6250c6e0baf536e504da71f915
EUIOwO <138953942+EUIOwO@users.noreply.github.com>
2025-03-12 18:15:25 +0800

    完成了《获取指定文件目录下的文件和文件夹》功能

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -1,9 +1,14 @@
-﻿// RemoteCtrl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿// RemoteCtrlCtrl.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 
 #include "pch.h"
 #include "framework.h"
 #include "RemoteCtrl.h"
+#include"Resource.h"
+#include "ServerSocket.h"
+#include<direct.h>
+#include<io.h>
+#include<list>
+
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -15,6 +20,88 @@
 CWinApp theApp;
 
 using namespace std;
+void Dump(BYTE* pData, size_t nSize) {
+    std::string strOut;
+    for (size_t i = 0; i < nSize; i++) {
+        char buf[8] = "";
+        if (i > 0 && (i % 16 == 0)) strOut += "\n";
+        snprintf(buf, sizeof(buf), "%02X ", pData[i] & 0xFF);
+        strOut += buf;
+    }
+    strOut += "\n";
+    OutputDebugStringA(strOut.c_str());
+}
+
+int MakeDriverInfo() {//创建一个磁盘信息的分区（1==>A, 2==>B, 3==>C...26==>Z）
+    std::string result;
+    for (int i = 1; i <= 26; i++) {
+        if (_chdrive(i) == 0) {
+            if (result.size() > 0) {
+                result += ',';
+            }
+            result += 'A' + i - 1;
+        }
+    }
+    CPacket pack(1, (BYTE*)result.c_str(), result.size());//打包
+    Dump((BYTE*)pack.Data(), pack.Size());//封装打包过程
+    //CServerSocket::getInstance()->Send(pack);
+
+    return 0;
+}
+
+typedef struct file_info {
+    file_info() {
+        IsInvalid = FALSE;
+        IsDirectory = -1;//无效
+        HasNext = TRUE;
+        memset(szFileName, 0, sizeof(szFileName));
+    }
+    BOOL IsInvalid;//是否有效
+    BOOL IsDirectory;//是否为目录 0 否 1 是
+    BOOL HasNext;//是否还有后续 0 没有 1 有
+    char szFileName[256];//文件名
+}FILEINFO, * PFILEINFO;
+
+int MakeDirectoryInfo() {
+    std::string strPath;
+    //std::list<FILEINFO>listFileInfos;
+    if (CServerSocket::getInstance()->GetFilePath(strPath) == false) {
+        OutputDebugString(_T("当前的命令，不是获取文件列表，命令解析错误！！"));
+        return -1;
+    }
+    if (_chdir(strPath.c_str()) != 0) {
+        FILEINFO finfo;
+        finfo.IsInvalid = TRUE;
+        finfo.IsDirectory = TRUE;
+        finfo.HasNext = FALSE;
+        memcpy(finfo.szFileName, strPath.c_str(), strPath.size());
+        //listFileInfos.push_back(finfo);
+        CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+        CServerSocket::getInstance()->Send(pack);
+        OutputDebugString(_T("没有权限访问目录！！"));
+        return -2;
+    }
+    _finddata_t fdata;
+    int hfind = 0;
+    if ((hfind = _findfirst("*", &fdata)) == -1) {
+        OutputDebugString(_T("没有找到任何文件！！"));
+        return -3;
+    }
+    do {
+        FILEINFO finfo;
+        finfo.IsDirectory = (fdata.attrib & _A_SUBDIR) != 0;
+        memcpy(finfo.szFileName, fdata.name, strlen(fdata.name));
+        //listFileInfos.push_back(finfo);
+        CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+        CServerSocket::getInstance()->Send(pack);
+    } while (!_findnext(hfind, &fdata));
+    //发送信息到控制端
+    FILEINFO finfo;
+    finfo.HasNext = FALSE;
+    CPacket pack(2, (BYTE*)&finfo, sizeof(finfo));
+    CServerSocket::getInstance()->Send(pack);
+    return 0;
+}
 
 int main()
 {
@@ -33,7 +120,38 @@ int main()
         }
         else
         {
-            // TODO: 在此处为应用程序的行为编写代码。
+            // TODO: socket, bind, listen, accept, read, write, close
+            //套接字初始化
+            //CServerSocket* pserver = CServerSocket::getInstance();
+            //int count = 0;
+
+            //if (pserver->initSocket() == false) {
+            //    MessageBox(NULL, _T("网络初始化异常， 未能成功初始化， 请检查网络状态"), _T("网络初始化失败"), MB_OK | MB_ICONERROR);//父窗口句柄，对话框文本，对话框标题，对话框类型
+            //    exit(0);
+            //}
+            //while (CServerSocket::getInstance() != NULL) {
+            //    if (pserver->AcceptClient() == false) {
+            //        if (count >= 3) {
+            //            MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
+            //            exit(0);
+            //        }
+            //        MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败"), MB_OK | MB_ICONERROR);
+            //        count++;
+            //    }
+            //    int ret = pserver->DealCommand();
+            //    //TODO:处理命令
+            // }
+            ////全局的静态变量
+            int nCmd = 1;
+            switch (nCmd) {
+            case 1://查看磁盘分区
+                MakeDriverInfo();
+                break;
+
+            case 2://查看指定目录下的文件
+                MakeDirectoryInfo();
+                break;
+            }
         }
     }
     else
---
+++ RemoteCtrl/RemoteCtrl/ServerSocket.h
@@ -0,0 +1,236 @@
+#pragma once
+#include "pch.h"
+#include "framework.h"
+
+class CPacket {
+public:
+	CPacket() :sHead(0), nLength(0), sCmd(0), sSum(0) {}
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize) {
+		sHead = 0xFEFF;
+		nLength = nSize + 4;
+		sCmd = nCmd;
+		strData.resize(nSize);
+		memcpy((void*)strData.c_str(), pData, nSize);
+
+		sSum = 0;
+		for (size_t j = 0; j < strData.size(); j++) {
+			sSum += BYTE(strData[j]) & 0xFF;
+		}
+	}
+	CPacket(const CPacket& pack) { //复制构造函数
+		sHead = pack.sHead;
+		nLength = pack.nLength;
+		sCmd = pack.sCmd;
+		strData = pack.strData;
+		sSum = pack.sSum;
+	}
+
+	CPacket(const BYTE* pData, size_t& nSize) {
+		size_t i = 0;
+		for (; i < nSize; i++) {
+			if (*(WORD*)(pData + i) == 0xFEFF) {
+				sHead = *(WORD*)(pData + i);
+				i += 2;
+				break;
+			}
+		}
+		if (i + 8 > nSize) {//包数据可能不全，或者包头未能全部接收到
+			nSize = 0;
+			return;
+		}
+		nLength = *(DWORD*)(pData + i); i += 4;
+
+		if (nLength + i > nSize) {//包没有完全接收到，就返回，解析失败
+			nSize = 0;
+			return;
+		}
+
+		sCmd = *(WORD*)(pData + i); i += 2;
+		if (nLength > 4) {
+			strData.resize(nLength - 2 - 2);
+			memcpy((void*)strData.c_str(), pData + i, nLength - 4);
+			i += nLength - 4;
+		}
+		sSum = *(WORD*)(pData + i); i += 2;
+		WORD sum = 0;
+		for (size_t j = 0; j < strData.size(); j++) {
+			sum += BYTE(strData[j]) & 0xFF;
+		}
+		if (sum == sSum) {
+			nSize = i;
+		}
+		nSize = 0;
+	}
+	~CPacket() {}
+	CPacket& operator = (const CPacket& pack) {//等于号的运算符重载
+		if (this != &pack) {
+			sHead = pack.sHead;
+			nLength = pack.nLength;
+			sCmd = pack.sCmd;
+			strData = pack.strData;
+			sSum = pack.sSum;
+		}
+		return *this;
+	}
+	int Size() {//获得包数据的大小
+		return nLength + 6;
+	}
+	const char* Data() {
+		strOut.resize(nLength + 6);
+		BYTE* pData = (BYTE*)strOut.c_str();
+		*(WORD*)pData = sHead; pData += 2;
+		*(DWORD*)(pData) = nLength; pData += 4;
+		*(WORD*)pData = sCmd; pData += 2;
+		memcpy(pData, strData.c_str(), strData.size()); pData += strData.size();
+		*(WORD*)pData = sSum;
+		return strOut.c_str();
+	}
+
+public:
+	WORD sHead;//固定位FE FF
+	DWORD nLength;//包长度（从控制命令开始，到和校验结束）
+	WORD sCmd;//控制命令
+	std::string strData;//包数据
+	WORD sSum;//和校验
+	std::string strOut;//整个包的数据
+};
+
+
+class CServerSocket
+{
+public:
+	static CServerSocket* getInstance() {
+		if (m_instance == NULL) {//静态函数没有 this 指针，所以无法直接访问成员变量
+			m_instance = new CServerSocket();
+		}
+		return m_instance;
+	}
+	bool initSocket() {
+
+		if (m_socket == -1) return false;
+		//TODO：校验
+		sockaddr_in serv_addr;
+		memset(&serv_addr, 0, sizeof(serv_addr));
+		serv_addr.sin_family = AF_INET;//地址族
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(9527);
+
+		//绑定
+		if (bind(m_socket, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+			return false;
+		}
+		if (listen(m_socket, 1) == -1) {
+			return false;
+		}
+		return true;
+
+
+	}
+	bool AcceptClient() {
+		sockaddr_in client_addr;
+		char buffer[1024]{};
+		int cli_sz = sizeof(client_addr);
+		m_client = accept(m_socket, (sockaddr*)&client_addr, &cli_sz);
+		if (m_client == -1) return false;
+		return true;
+
+		/*recv(client, buffer, sizeof(buffer), 0);
+		send(client, buffer, sizeof(buffer), 0);*/
+	}
+#define BUFFER_SIZE 4096
+	int DealCommand() {
+		if (m_client == -1) return -1;
+		//char buffer[1024] = "";
+		char* buffer = new char[4096];
+		memset(buffer, 0, 4096);
+		size_t index = 0;
+		while (1) {
+			size_t len = recv(m_client, buffer + index, BUFFER_SIZE - static_cast<size_t>(index), 0);
+			if (len <= 0) {
+				return -1;
+			}
+			index += len;
+			len = index;
+			m_packet = CPacket((BYTE*)buffer, len);
+			if (len > 0) {
+				memmove(buffer, buffer + len, 4096 - len);
+				index -= len;
+				return m_packet.sCmd;
+			}
+		}
+		return -1;
+	}
+
+	bool Send(const char* pData, size_t nSize) {
+		if (m_client == -1) return false;
+		return send(m_client, pData, (int)nSize, 0) > 0;
+	}
+
+	bool Send(CPacket& pack) {
+		if (m_client == -1) return false;
+		return send(m_client, pack.Data(), pack.Size(), 0) > 0;
+	}
+
+	bool GetFilePath(std::string& strPath) {
+		if (m_packet.sCmd == 2) {
+			strPath = m_packet.strData;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	SOCKET m_client;
+	SOCKET m_socket;
+	CPacket m_packet;
+	CServerSocket& operator = (const CServerSocket& ss) {}
+	CServerSocket(const CServerSocket& ss) {
+		m_socket = ss.m_socket;
+		m_client = ss.m_client;
+	}
+	CServerSocket() {
+
+		m_client = INVALID_SOCKET;
+		if (InitSocEnv() == FALSE) {
+			MessageBox(NULL, _T("无法初始化套接字环境, 请检查网络设置！"), _T("初始化错误！"), MB_OK | MB_ICONERROR);
+			exit(0);
+		}
+		SOCKET m_socket = socket(PF_INET, SOCK_STREAM, 0);
+	}
+	~CServerSocket() {
+		closesocket(m_socket);
+		WSACleanup();
+	}
+
+	BOOL InitSocEnv() {
+		WSADATA data;
+		if (WSAStartup(MAKEWORD(1, 1), &data) != 0) {
+			return FALSE;
+
+		}
+		return TRUE;
+	}
+
+	void releaseInstance() {
+		if (m_instance != NULL) {
+			CServerSocket* tmp = m_instance;
+			m_instance = NULL;
+			delete tmp;
+		}
+	}
+
+	static CServerSocket* m_instance;
+public:
+	class CHelper {
+	public:
+		CHelper() {
+			CServerSocket::getInstance();
+		}
+		~CHelper() {
+
+		}
+	};
+};
+
+//声明外部变量
+extern CServerSocket server;
